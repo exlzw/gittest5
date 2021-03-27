2d1a88384aaa71392a90a695e7249b14ba9ebf96
lzw <1340569613@qq.com>
2021-03-27 13:22:07 +0800

    提交1

--- app/src/main/java/com/example/gittest5/MainActivity.java
+++ app/src/main/java/com/example/gittest5/MainActivity.java
@@ -10,8 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-
+  ///提交1
         setContentView(R.layout.activity_main);
     }
 }
